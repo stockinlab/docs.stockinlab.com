8f68f79c37e324a3ee83a5972ecd8dab12752206
Ludo <ludo@stockinlab.com>
2020-05-13 21:48:17 +0800

    feat: cleanup unused packages and theme

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -1,21 +1,7 @@
-const path = require('path');
-
 module.exports = (options, ctx) => {
-    const {themeConfig, siteConfig} = ctx;
-
-    const isAlgoliaSearch = (
-        themeConfig.algolia
-        || Object
-            .keys(siteConfig.locales && themeConfig.locales || {})
-            .some(base => themeConfig.locales[base].algolia)
-    );
-
-    const enableSmoothScroll = themeConfig.smoothScroll === true;
-
     return {
         plugins: [
             ['@vuepress/active-header-links', options.activeHeaderLinks],
-            '@vuepress/search',
             '@vuepress/plugin-nprogress',
             '@vuepress/medium-zoom',
             // ['@vuepress/pwa', {
@@ -58,7 +44,7 @@ module.exports = (options, ctx) => {
                 before: info => `<div class="custom-block danger"><div class="custom-block-title"><svg class="inline-flex h-5 w-5 mr-2 text-red-400"><use href="/assets/images/icons.svg#error"/></svg>${info}</div>\n`,
                 after: () => '</div>\n',
             }],
-            ['smooth-scroll', enableSmoothScroll],
+            'smooth-scroll',
         ],
     };
 };
